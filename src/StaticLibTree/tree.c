56dcaa7b5f3e82305038b47563c58d5d85763480
sanai371 <g2027021@st.t-kougei.ac.jp>
2020-11-15 11:07:03 +0900

    リストの生成と削除を実装しました。

--- src/StaticLibTree/tree.c
+++ src/StaticLibTree/tree.c
@@ -10,11 +10,23 @@ void initialize(binary_tree* t)
 	t->root = NULL;
 }
 
+void free_recursive(node* p) {
+	if (p == NULL)return;
+	free_recursive(p->left);
+	free_recursive(p->right);
+	free(p);
+}
+
 //
 // 使用メモリの全解放
 //
 void finalize(binary_tree* t)
 {
+	free_recursive(t->root);
+}
+
+static void insert_node(node* p, node* n) {
+
 }
 
 //
@@ -23,16 +35,38 @@ void finalize(binary_tree* t)
 bool add_node(binary_tree* t, int key, const char* value)
 {
 	// todo: ノードを作成して、適切な位置に挿入してください
+	node* p = malloc(sizeof(node));
+	if (p == NULL) return false;
+
+	p->key = key;
+	strcpy_s(p->value,256,value);
+	p->right = p->left = NULL;
+
+	if (t->root = NULL) {
+		t->root = p;
+	}
+	else {
+	}
 
 	return true;
 }
+
+static void search_nodes(const node* p,void (*func)(const node* p)) {
+	if (p == NULL)return false;
+	search_nodes(p->left, func);
+	func(p);
+	search_nodes(p->right, func);
+}
+
 
 //
 // keyの小さな順にコールバック関数funcを呼び出す
 //
 void serach(const binary_tree* t, void (*func)(const node* p))
 {
+	search_nodes(t->root, func);
+	
 	// todo: keyの小さな順にfunc関数をよびだしてください
-	func(t->root);// テストが通ってしまっていたので、通らなくした
+	
 }
 
